e479ba4c8a84aed62be4d2b8ececa593984d2f7d
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-01 11:31:46 -0600

    tests: fix integration test setup

--- tests/FiscalOS.API.Tests/Usings.cs
+++ tests/FiscalOS.API.Tests/Usings.cs
@@ -1,11 +1,14 @@
 global using System.Net;
 global using System.Net.Http.Json;
 
+global using FiscalOS.API.Data;
 global using FiscalOS.API.Tests.Infra;
 
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 
 global using Xunit.Sdk;
