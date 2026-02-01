2b91e3b791239ef0cefa2a3f22209c43283e7b74
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-01 06:12:09 -0600

    refactor: use ivalidatableobject api

--- src/FiscalOS.API/Login/Request.cs
+++ src/FiscalOS.API/Login/Request.cs
@@ -1,8 +1,21 @@
+
 namespace FiscalOS.API.Login;
 
-public record LoginRequest(
-  [Required]
-  string Username,
-  [Required]
-  string Password
-);
+public record LoginRequest : IValidatableObject
+{
+  public string Username { get; init; } = string.Empty;
+  public string Password { get; init; } = string.Empty;
+
+  public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+  {
+    if (string.IsNullOrWhiteSpace(Username))
+    {
+      yield return new("The Username field is required.", [nameof(Username)]);
+    }
+
+    if (string.IsNullOrWhiteSpace(Password))
+    {
+      yield return new("The Password field is required.", [nameof(Password)]);
+    }
+  }
+}
