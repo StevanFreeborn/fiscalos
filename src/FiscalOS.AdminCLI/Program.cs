e03cc0403a65c1beadced0d543d7c31d4ee0273a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-02 07:00:37 -0600

    chore: add todo with psuedo code

--- src/FiscalOS.AdminCLI/Program.cs
+++ src/FiscalOS.AdminCLI/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// TODO: We need a way to add
+// a user to the system with
+// a properly hashed password
+// so need something like this:
+//
+// 1. Ask for username
+// 2. Ask for user password
+// 3. Hash user password
+// 4. Save user to database
+
+Console.WriteLine("Hello, World!");
