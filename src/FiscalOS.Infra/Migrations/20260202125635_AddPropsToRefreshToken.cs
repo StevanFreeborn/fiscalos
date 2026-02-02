3a1a7138c5c1d50e779646902ec4d53700305acb
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-02 07:00:28 -0600

    feat(infra): update refresh token model

---
+++ src/FiscalOS.Infra/Migrations/20260202125635_AddPropsToRefreshToken.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FiscalOS.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropsToRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ExpiresAt",
+                table: "RefreshTokens",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "RefreshTokens",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiresAt",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "RefreshTokens");
+        }
+    }
+}
