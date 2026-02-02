d9f0585c4cad988c6ae27c04d9a2a1f62105798c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-02 15:45:44 -0600

    feat(infra): add indexes with uniqueness

---
+++ src/FiscalOS.Infra/Migrations/20260202214513_AddIndexesWithUniquenessToModels.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FiscalOS.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesWithUniquenessToModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens");
+        }
+    }
+}
