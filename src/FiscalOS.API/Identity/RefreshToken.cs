7e67e02a6890c8d8a3e4723ca9141400b72f4658
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-02 04:45:56 -0600

    refactor(core): move identity models from API to Core

--- src/FiscalOS.API/Identity/RefreshToken.cs
+++
@@ -1,9 +0,0 @@
-namespace FiscalOS.API.Identity;
-
-internal sealed class RefreshToken
-{
-  public Guid Id { get; init; }
-
-  public Guid UserId { get; init; }
-  public User User { get; init; } = new();
-}
