3fbd9402c38918267adfb0e619d0ba912ce607a0
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-03 11:06:44 -0600

    chore: remove OpenApi and configure JWT authentication schemes

--- src/FiscalOS.Infra/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/FiscalOS.Infra/DependencyInjection/ServiceCollectionExtensions.cs
@@ -5,12 +5,17 @@ public static class ServiceCollectionExtensions
   public static IServiceCollection AddInfrastructure(this IServiceCollection services)
   {
     services.AddSingleton(TimeProvider.System);
+
     services.ConfigureOptions<JwtOptionsSetup>();
+    services.ConfigureOptions<JwtBearerOptionsSetup>();
+
     services.AddSingleton<ITokenGenerator, TokenGenerator>(TokenGenerator.From);
     services.AddSingleton<IPasswordHasher, PasswordHasher>(PasswordHasher.From);
+
     services.ConfigureOptions<AppDbContextOptionsSetup>();
     services.AddDbContext<AppDbContext>();
     services.AddHostedService<MigrationService>();
+
     return services;
   }
 }
