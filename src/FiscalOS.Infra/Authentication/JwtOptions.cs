d1a4de8fdb6cd0eb1d2903d5616abafa4f4d7084
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-03 11:06:44 -0600

    chore: remove OpenApi and configure JWT authentication schemes

--- src/FiscalOS.Infra/Authentication/JwtOptions.cs
+++ src/FiscalOS.Infra/Authentication/JwtOptions.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 namespace FiscalOS.Infra.Authentication;
 
 public sealed record JwtOptions
@@ -6,6 +8,8 @@ public sealed record JwtOptions
   public string Audience { get; init; } = string.Empty;
   public string Secret { get; init; } = string.Empty;
   public int ExpiryInMinutes { get; init; } = 5;
+
+  public SymmetricSecurityKey Key => new(Encoding.UTF8.GetBytes(Secret));
 }
 
 public sealed record JwtOptionsSetup : IConfigureOptions<JwtOptions>
@@ -21,5 +25,34 @@ public sealed record JwtOptionsSetup : IConfigureOptions<JwtOptions>
   public void Configure(JwtOptions options)
   {
     _configuration.GetSection(SectionName).Bind(options);
+  }
+}
+
+public sealed class JwtBearerOptionsSetup(IOptions<JwtOptions> jwtOptions) : IConfigureNamedOptions<JwtBearerOptions>
+{
+  private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+
+  public void Configure(string? name, JwtBearerOptions options)
+  {
+    options.TokenValidationParameters = new()
+    {
+      ValidIssuer = _jwtOptions.Issuer,
+      ValidAudience = _jwtOptions.Audience,
+      IssuerSigningKey = _jwtOptions.Key,
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidateIssuerSigningKey = true,
+      ValidateLifetime = true,
+    };
+
+    if (name is Schemes.AllowExpiredTokens)
+    {
+      options.TokenValidationParameters.ValidateLifetime = false;
+    }
+  }
+
+  public void Configure(JwtBearerOptions options)
+  {
+    Configure(Options.DefaultName, options);
   }
 }
