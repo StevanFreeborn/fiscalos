34102e451df43128fdab96192c78b04829405319
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-06 06:43:55 -0600

    feat(infra): added migrations for adding properties to user model

--- src/FiscalOS.Infra/Migrations/20260205192220_AddEncryptionKeyToUserModel.Designer.cs
+++ src/FiscalOS.Infra/Migrations/20260205192220_AddEncryptionKeyToUserModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FiscalOS.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FiscalOS.Infra.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260205192220_AddEncryptionKeyToUserModel")]
+    partial class AddEncryptionKeyToUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
@@ -62,10 +65,6 @@ namespace FiscalOS.Infra.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<string>("EncryptedDataKey")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("EncryptionKeyId")
                         .IsRequired()
                         .HasColumnType("TEXT");
 
