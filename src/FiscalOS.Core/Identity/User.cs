6fdbe146c91ce7fc64a4a8ce5feec3f7dfe604c2
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-06 06:44:24 -0600

    feat(core): extend user model with encryption related properties

--- src/FiscalOS.Core/Identity/User.cs
+++ src/FiscalOS.Core/Identity/User.cs
@@ -6,6 +6,8 @@ public sealed class User : Entity
 
   public string Username { get; init; } = string.Empty;
   public string HashedPassword { get; init; } = string.Empty;
+  public string EncryptionKeyId { get; init; } = string.Empty;
+  public string EncryptedDataKey { get; init; } = string.Empty;
 
   public IEnumerable<RefreshToken> RefreshTokens => _refreshTokens;
 
@@ -18,12 +20,18 @@ public sealed class User : Entity
     return new();
   }
 
-  public static User From(string username, string hashedPassword)
+  public static User From(string username, string hashedPassword, EncryptedDataKey encryptedDataKey)
   {
+    ArgumentNullException.ThrowIfNull(username);
+    ArgumentNullException.ThrowIfNull(hashedPassword);
+    ArgumentNullException.ThrowIfNull(encryptedDataKey);
+
     return new()
     {
       Username = username,
       HashedPassword = hashedPassword,
+      EncryptedDataKey = encryptedDataKey.EncryptedKey,
+      EncryptionKeyId = encryptedDataKey.KeyIdUsed,
     };
   }
 
