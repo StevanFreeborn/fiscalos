97c7847a8937c9f2d9daf4d1914d3b9fc382e0f5
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-06 06:45:10 -0600

    chore: removed unnecessary usings

--- src/FiscalOS.Infra/Authentication/JwtOptions.cs
+++ src/FiscalOS.Infra/Authentication/JwtOptions.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-
 namespace FiscalOS.Infra.Authentication;
 
 public sealed record JwtOptions
--- src/FiscalOS.Infra/Authentication/Schemes.cs
+++ src/FiscalOS.Infra/Authentication/Schemes.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-
 namespace FiscalOS.Infra.Authentication;
 
 public static class Schemes
