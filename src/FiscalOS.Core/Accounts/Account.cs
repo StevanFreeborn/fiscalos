a2e9fc3f306b18ab5a477846a2a6ada73f2a6352
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-13 12:03:07 -0600

    fix(core): fix incorrectly named property

--- src/FiscalOS.Core/Accounts/Account.cs
+++ src/FiscalOS.Core/Accounts/Account.cs
@@ -3,7 +3,7 @@ namespace FiscalOS.Core.Accounts;
 public sealed class Account : Entity
 {
   public Guid UserId { get; init; }
-  public Guid InstituionId { get; init; }
+  public Guid InstitutionId { get; init; }
   public Institution Institution { get; init; } = null!;
   public string Name { get; init; } = string.Empty;
   public AccountMetadata Metadata { get; init; } = null!;
@@ -12,7 +12,7 @@ public sealed class Account : Entity
   {
     return new()
     {
-      InstituionId = institutionId,
+      InstitutionId = institutionId,
       Name = name,
       Metadata = accountMetadata,
     };
