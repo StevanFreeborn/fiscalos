15f05fc607f4930a9f2d34324f9dba0a66d0ac8c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-14 06:44:17 -0600

    feat(infra): added modeling and migrations for currency code prop

--- src/FiscalOS.Infra/Migrations/20260214122622_AddCurrencyCodeToBalanceModel.Designer.cs
+++ src/FiscalOS.Infra/Migrations/20260214122622_AddCurrencyCodeToBalanceModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FiscalOS.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FiscalOS.Infra.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260214122622_AddCurrencyCodeToBalanceModel")]
+    partial class AddCurrencyCodeToBalanceModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
---
+++ src/FiscalOS.Infra/Migrations/20260214122622_AddCurrencyCodeToBalanceModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FiscalOS.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrencyCodeToBalanceModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencyCode",
+                table: "Balance",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyCode",
+                table: "Balance");
+        }
+    }
+}
