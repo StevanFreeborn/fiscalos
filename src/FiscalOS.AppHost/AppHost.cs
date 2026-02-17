df859b91b01c04b67f07ad1b87c1eafd6be23370
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-02-17 06:18:18 -0600

    feat: add frontend with aspire integration

--- src/FiscalOS.AppHost/AppHost.cs
+++ src/FiscalOS.AppHost/AppHost.cs
@@ -3,4 +3,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<Projects.FiscalOS_API>("API")
   .WithHttpHealthCheck("/health");
 
+builder.AddViteApp("web", "../FiscalOS.Web")
+  .WithReference(api)
+  .WithHttpsEndpoint(port: 7061, env: "PORT");
+
 builder.Build().Run();
