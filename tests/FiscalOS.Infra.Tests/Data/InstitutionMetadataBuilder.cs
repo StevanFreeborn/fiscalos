659b2bbf191d6c6f0b667f70b068c9bcb0e7ea74
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-03-06 07:14:57 -0600

    tests(infra.tests): wip on builder apis for test data

---
+++ tests/FiscalOS.Infra.Tests/Data/InstitutionMetadataBuilder.cs
@@ -0,0 +1,52 @@
+namespace FiscalOS.Infra.Tests.Data;
+
+internal sealed class InstitutionMetadataBuilder
+{
+  private string _plaidId = "plaidId";
+  private string _plaidName = "plaidName";
+  private string _accessToken = "encryptedAccessToken";
+  private string _itemId = "itemId";
+
+  private InstitutionMetadataBuilder()
+  {
+  }
+
+  public static InstitutionMetadataBuilder Create()
+  {
+    return new();
+  }
+
+  public InstitutionMetadataBuilder WithPlaidId(string plaidId)
+  {
+    _plaidId = plaidId;
+    return this;
+  }
+
+  public InstitutionMetadataBuilder WithPlaidName(string plaidName)
+  {
+    _plaidName = plaidName;
+    return this;
+  }
+
+  public InstitutionMetadataBuilder WithAccessToken(string accessToken)
+  {
+    _accessToken = accessToken;
+    return this;
+  }
+
+  public InstitutionMetadataBuilder WithItemId(string itemId)
+  {
+    _itemId = itemId;
+    return this;
+  }
+
+  public PlaidInstitutionMetadata Build()
+  {
+    return PlaidInstitutionMetadata.From(
+      _plaidId,
+      _plaidName,
+      _accessToken,
+      _itemId
+    );
+  }
+}
